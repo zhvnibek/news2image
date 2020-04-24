60f0563cfdd16f85ddb463657ac85e78ea4ab5b2
Zhanibek <38990635+zhvnibek@users.noreply.github.com>
2020-04-25 00:47:23 +0600

    Update bot_config.py

--- src/tg_bot/bot_config.py
+++ src/tg_bot/bot_config.py
@@ -11,6 +11,6 @@ logging.basicConfig(format='%(levelname)s [%(asctime)s] [%(name)s] %(message)s',
 logger = logging.getLogger(__name__)
 
 parse_mode = ParseMode.MARKDOWN
-TOKEN = os.environ.get('TOKEN', '1076785258:AAGIQxaVrcvadScmR7DHwOaXavSSF_P9HQs')
+TOKEN = os.environ.get('TOKEN')
 updater = Updater(token=TOKEN, use_context=True)
 bot = telegram.Bot(token=TOKEN)
